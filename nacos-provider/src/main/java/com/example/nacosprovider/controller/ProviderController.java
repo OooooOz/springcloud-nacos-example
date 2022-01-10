880e472768501fe05673ad66c12a8ec54d2f52be
OooooOz <zhongwenhao@gerpgo.com>
2022-01-10 15:29:50 +0800

    yaml调整

--- nacos-provider/src/main/java/com/example/nacosprovider/controller/ProviderController.java
+++ nacos-provider/src/main/java/com/example/nacosprovider/controller/ProviderController.java
@@ -8,18 +8,17 @@ import com.alibaba.nacos.api.naming.NamingService;
 import com.alibaba.nacos.api.naming.pojo.Instance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 @RequestMapping("/provider")
-@Controller
+@RestController
 class ProviderController {
 
     @Value("${name}")
@@ -35,14 +34,12 @@ class ProviderController {
     private NacosDiscoveryProperties nacosDiscoveryProperties;
 
     @GetMapping("/getValue")
-    @ResponseBody
     public String getValue(){
         return "[name: " + name + "]";
     }
 
 
     @RequestMapping(value = "/getService", method = GET)
-    @ResponseBody
     public List<Instance> get(@RequestParam String serviceName) throws NacosException {
         // namingService注入为null,采用nacosServiceManager获取namingService注入为null
         namingService =  nacosServiceManager.getNamingService(nacosDiscoveryProperties.getNacosProperties());
