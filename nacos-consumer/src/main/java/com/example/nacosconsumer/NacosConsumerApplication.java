7a7041da89e6c19085804441d1623ef05ae91066
zhongwenhao <zhongwenhao@gerpgo.com>
2022-07-19 18:13:04 +0800

    nacos

--- nacos-consumer/src/main/java/com/example/nacosconsumer/NacosConsumerApplication.java
+++ nacos-consumer/src/main/java/com/example/nacosconsumer/NacosConsumerApplication.java
@@ -2,9 +2,11 @@ package com.example.nacosconsumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
+// 不需要数据库，避免自动配置启动报错
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @EnableFeignClients
 public class NacosConsumerApplication {
 
--- nacos-provider/src/main/java/com/example/nacosprovider/NacosProviderApplication.java
+++ nacos-provider/src/main/java/com/example/nacosprovider/NacosProviderApplication.java
@@ -2,8 +2,9 @@ package com.example.nacosprovider;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class NacosProviderApplication {
 
     public static void main(String[] args) {
