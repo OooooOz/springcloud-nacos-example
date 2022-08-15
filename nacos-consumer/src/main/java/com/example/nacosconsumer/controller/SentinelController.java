16a42f39abb7eb17d9a3324a5d034f8bcd7fe473
zhongwenhao <zhongwenhao@gerpgo.com>
2022-08-15 15:27:16 +0800

    sentinel

--- nacos-consumer/src/main/java/com/example/nacosconsumer/controller/SentinelController.java
+++ nacos-consumer/src/main/java/com/example/nacosconsumer/controller/SentinelController.java
@@ -1,10 +1,12 @@
 package com.example.nacosconsumer.controller;
 
 import com.alibaba.csp.sentinel.annotation.SentinelResource;
-import com.sun.deploy.security.BlockedException;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping("/test")
@@ -13,6 +15,11 @@ public class SentinelController {
     @GetMapping("/add")
     @SentinelResource(value = "/test/add", blockHandler = "blockHandlerMethod")
     public String add() {
+        try {
+            TimeUnit.SECONDS.sleep(2);
+        } catch (InterruptedException e) {
+            System.out.println("==========================");
+        }
         return "add";
     }
 
@@ -21,7 +28,7 @@ public class SentinelController {
         return "get";
     }
 
-    public String blockHandlerMethod(BlockedException e) {
+    public String blockHandlerMethod(BlockException e) {
         return "流控";
     }
 }
